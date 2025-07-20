15e6c5a4ac652f8e078be273d6f1f27786960312
Lopez-CL <cllopez511@gmail.com>
2025-07-20 16:46:38 -0400

    created user model

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/users_db',{
-    useNewUrlParser: true,
-    useUnifiedTypology: true
-})
-.then(()=> console.log("Established connection to database"))
-.catch(err=> console.log(err, "Issue connecting with data base"))
+mongoose.connect("mongodb://127.0.0.1:27017/reelRythms_db")
+    .then(()=> console.log("Established connection to the user database!"))
+    .catch(err => console.log(err, "Issue with connecting to database"))
---
+++ server/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+const UserSchema = new mongoose.Schema({
+    userName:{
+        type: String,
+        minLength: [2, "Username must be two characters or more."],
+        required: [true, 'username required to create an account!'],
+    },
+    email:{
+            type: String,
+            required: [true, "email required to create an account!"],
+            validator: val => /^([\w\-/.]+@([\w\-]+\.+[\w\-]+)$)/.test(val),
+            message: "Invalid entry for eamil. Try again."
+    },
+    password:{
+        type: String,
+        required: [true, "password required to create an account"],
+        minLength: [8, "Passoword must be 8 characters of longer"]
+    },
+    confirmPassword:{
+        type: String,
+        required: [true, "please confrim password"],
+    },
+    profImg:{
+        type: Buffer
+    }
+}, 
+{ timestamps: true });
+
+//middleware and virtual field creation
+UserSchema.pre('validate', function(next){
+    if(this.password !== this._confirmPassword) this.invalidate("password","password do not match, back-end Error");
+    next();
+})
+UserSchema.virtual('confirmPassword')
+    .get(() => this._confirmPassword)
+    .set(val => this._confirmPassword = val);
+UserSchema.pre('save', async function(next){
+    try{
+        const pwToHash = await bcrypt.hash(this.password, 12);
+        this.password = pwToHash;
+    }catch(err){
+        console.log(err, "Issue with registration")
+    }
+    next();
+})
+UserSchema.virtual("filmCalendars",{
+    ref: "FilmCalendar",
+    localField: '_id',
+    foreignField: "user",
+    justOne: false
+})
+UserSchema.set('toObject', {virtuals: true})
+UserSchema.set('toJSON',{virtuals:true})
+
+module.exports = mongoose.Model('User', UserSchema);
