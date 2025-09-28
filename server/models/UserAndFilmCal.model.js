46ccd93e6e65045c1a2faf56b3dbb309379d7c93
Lopez-CL <cllopez511@gmail.com>
2025-09-28 14:38:17 -0400

    next.js changes

--- server/models/UserAndFilmCal.model.js
+++
@@ -1,8 +0,0 @@
-const {Types, model, Schema} = require('mongoose');
-const UserAndFilmCalJoinSchema = new Schema({
-    user:{type: Types.ObjectId, ref:'User', required: true},
-    filmCalendar:{type: Types.ObjectId, ref:'FilmCalendar', required: true}
-}, {timestamps:true});
-
-UserAndFilmCalJoinSchema.index({ user: 1, filmCalendar: 1 }, { unique: true });
-module.exports = model('UserAndFilmCalJoin', UserAndFilmCalJoinSchema);
--- server/routes/filmCalendar.routes.js
+++
@@ -1,8 +0,0 @@
-const FilmCalendarController = require('../controllers/filmCalendar.controller');
-const {authenticate} = require('../config/jwt.config');
-
-module.exports = app =>{
-    app.post('/api/createfilmCal', authenticate, FilmCalendarController.createCalendar)
-    app.get('/api/getfilmCals', authenticate, FilmCalendarController.getAllFilmCalendars)
-    app.get('/api/filmCal/:_id', authenticate, FilmCalendarController.getAFilmCalendar)
-}
